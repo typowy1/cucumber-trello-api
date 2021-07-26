9a8031a189e44b05634ac7787c6e9b23560d4667
Bartosz Kita <bartosz.kita@gmail.com>
2021-07-26 20:54:49 +0200

    Added TrelloUrl class

---
+++ src/main/java/pl/akademiaqa/url/TrelloUrl.java
@@ -0,0 +1,11 @@
+package pl.akademiaqa.url;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class TrelloUrl {
+
+    public static final String BASE_URL = "https://api.trello.com/1/";
+    public static final String BOARDS = "boards/";
+}
